9244c7f27974100c071c3db55aae11cd5575fb59
ivansamudio <ivansamudio07@gmail.com>
2024-08-25 06:45:39 -0300

    add automatic theme detect

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@
  */
 
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
     if (scrollTop >= 80) {
         document.body.classList.add('fixed-header');
@@ -15,23 +15,24 @@ window.addEventListener("scroll", function() {
     }
 });
 
-function partialRender(htmlName,domContentName,section = "home"){
-    fetch("assets/html/"+ htmlName)
-            .then(response => response.text())
-            .then(data => {
-                // Insertar el contenido recibido en el contenedor
-                document.querySelector("."+domContentName).innerHTML = data;
-                if(section == "navbar"){
-                    document.getElementById("language-toggle").addEventListener('change',(e) => readLanguage(e));
-                    document.getElementById("mode-toggle").addEventListener('change',(e) => changeMode(e));
-                }else if(section == "carousel"){
-                    owlCarouselMain();
-                }
-            })
-            .catch(error => console.error('Error al cargar el contenido:', error));
+function partialRender(htmlName, domContentName, section = "home") {
+    fetch("assets/html/" + htmlName)
+        .then(response => response.text())
+        .then(data => {
+            // Insertar el contenido recibido en el contenedor
+            document.querySelector("." + domContentName).innerHTML = data;
+            if (section == "navbar") {
+                document.getElementById("language-toggle").addEventListener('change', (e) => readLanguage(e));
+                document.getElementById("mode-toggle").addEventListener('change', (e) => changeMode(e));
+                detectColorScheme();
+            } else if (section == "carousel") {
+                owlCarouselMain();
+            }
+        })
+        .catch(error => console.error('Error al cargar el contenido:', error));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Todo el DOM ha sido cargado y es seguro manipular los elementos del DOM aquí.
     runApp();
 });
@@ -41,38 +42,62 @@ document.addEventListener('DOMContentLoaded', function() {
 // Esta función inicializa la aplicación
 function runApp() {
     // Realiza el partial render de la NavBar
-    partialRender("navbar.html","main-header","navbar");
-    partialRender("home.html","home-section");
-    partialRender("about.html","about-section");
-    partialRender("tools.html","tools-section");
-    partialRender("skills.html","skill-section");
-    partialRender("portfolio.html","portfolio-section");
-    partialRender("portfolio.html","portfolio-section");
-    partialRender("social.html","social-section","carousel");
-    partialRender("contact.html","contact-section");
+    partialRender("navbar.html", "main-header", "navbar");
+    partialRender("home.html", "home-section");
+    partialRender("about.html", "about-section");
+    partialRender("tools.html", "tools-section");
+    partialRender("skills.html", "skill-section");
+    partialRender("portfolio.html", "portfolio-section");
+    partialRender("portfolio.html", "portfolio-section");
+    partialRender("social.html", "social-section", "carousel");
+    partialRender("contact.html", "contact-section");
     $.scrollIt({
         easing: 'linear',
         topOffset: -70
     });
-    
 }
 
-function owlCarouselMain(){
-    $(document).ready(function(){
+function detectColorScheme(){
+    var theme="light";    //default to light
+
+    //local storage is used to override OS theme settings
+    if(localStorage.getItem("theme")){
+        if(localStorage.getItem("theme") == "dark"){
+            
+            var theme = "dark";
+        }
+    } else if(!window.matchMedia) {
+        //matchMedia method not supported
+        return false;
+    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        //OS theme setting detected as dark
+        var theme = "dark";
+    }
+
+    //dark theme preferred, set document with a `data-theme` attribute
+    if (theme=="dark") {
+        document.getElementById('label-mode-toggle').innerHTML = "<i class='bx bx-sun'></i>";
+        document.documentElement.setAttribute("data-theme", "dark");
+        document.getElementById('mode-toggle').checked = true;
+    }
+}
+
+function owlCarouselMain() {
+    $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
-            loop:true,
+            loop: true,
             items: 2,
-            margin:30,
-            autoplay:true,
+            margin: 30,
+            autoplay: true,
             autoplayTimeout: 9000,
-            responsive:{
-                0:{
-                    items:1
+            responsive: {
+                0: {
+                    items: 1
                 },
-                900:{
-                    items:2
+                900: {
+                    items: 2
                 },
-                
+
             }
         });
     });
@@ -85,51 +110,55 @@ function copyEmail() {
     let text = "ivansamudio07@gmail.com";
     let notification = document.getElementById("notification");
 
-    navigator.clipboard.writeText(text).then(function() {
+    navigator.clipboard.writeText(text).then(function () {
         // Mostrar la notificación
         notification.classList.add("show");
 
         // Ocultar la notificación después de 2 segundos
-        setTimeout(function() {
+        setTimeout(function () {
             notification.classList.remove("show");
         }, 2000);
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.error('Error al copiar: ', error);
     });
 }
 
 
-function readLanguage(e){
+function readLanguage(e) {
     let checked = e.target.checked;
     let label_toggle = document.getElementById('label-language-toggle');
-    if(checked){
+    if (checked) {
         changeLenguage("en");
         label_toggle.innerHTML = '<i>ES</i>';
-    }else{
+    } else {
         changeLenguage("es");
         label_toggle.innerHTML = '<i>EN</i>';
-    }   
+    }
 }
 
-function changeMode(e){
+function changeMode(e) {
     let checked = e.target.checked;
     let label_toggle = document.getElementById('label-mode-toggle');
-    document.body.classList.toggle('dark-mode');
-    if(checked){
+    if (checked) {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
         label_toggle.innerHTML = "<i class='bx bx-sun' ></i>";
-    }else{
+        checked = true;
+    } else {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'light');
         label_toggle.innerHTML = "<i class='bx bx-moon' ></i>";
-        
-    }   
+        checked = false;
+    }
 }
 
-async function changeLenguage(language){
+async function changeLenguage(language) {
     const textsToChange = document.querySelectorAll("[data-section]");
-    const requestJson = await fetch('assets/lenguages/' + language  +'.json');
+    const requestJson = await fetch('assets/lenguages/' + language + '.json');
     const texts = await requestJson.json()
-    for (const textToChange of textsToChange){
+    for (const textToChange of textsToChange) {
         const section = textToChange.dataset.section;
         const value = textToChange.dataset.value;
-        textToChange.innerHTML= texts[section][value];
+        textToChange.innerHTML = texts[section][value];
     }
 } 
