35f9930f7604dacb16a66e864c1103bff48848a5
ivansamudio <ivansamudio07@gmail.com>
2024-08-25 07:09:14 -0300

    update theme function

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,24 +58,8 @@ function runApp() {
 }
 
 function detectColorScheme(){
-    var theme="light";    //default to light
-
-    //local storage is used to override OS theme settings
-    if(localStorage.getItem("theme")){
-        if(localStorage.getItem("theme") == "dark"){
-            
-            var theme = "dark";
-        }
-    } else if(!window.matchMedia) {
-        //matchMedia method not supported
-        return false;
-    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        //OS theme setting detected as dark
-        var theme = "dark";
-    }
-
-    //dark theme preferred, set document with a `data-theme` attribute
-    if (theme=="dark") {
+    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (darkMode) {
         document.getElementById('label-mode-toggle').innerHTML = "<i class='bx bx-sun'></i>";
         document.documentElement.setAttribute("data-theme", "dark");
         document.getElementById('mode-toggle').checked = true;
