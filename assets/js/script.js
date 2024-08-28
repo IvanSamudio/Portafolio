955e21882d2f8d81a761e6540b6bf7fb6cc0cd40
ivansamudio <ivansamudio07@gmail.com>
2024-08-27 22:32:56 -0300

    rapair bug´s and add a new project

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,38 @@
  * Autor: Ivan Samudio
  * Descripción: Este archivo maneja la lógica de la interfaz de usuario de la aplicación.
  */
-
-
-window.addEventListener("scroll", function () {
-    var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop >= 80) {
-        document.body.classList.add('fixed-header');
-    } else {
-        document.body.classList.remove('fixed-header');
+//FUNCIONES GENERALES
+//LEE EL LENGUAJE ACTUAL
+function readLanguage(isSpanish) {
+    changeLenguage(isSpanish ? "es" : "en");
+    document.getElementById('label-language-toggle').innerHTML = isSpanish ? '<i>EN</i>' : '<i>ES</i>'
+    document.getElementById('language-toggle').checked = isSpanish;
+}
+//REALIZA EL CAMBIO DE IDIOMA
+async function changeLenguage(language) {
+    const textsToChange = document.querySelectorAll("[data-section]");
+    const requestJson = await fetch('assets/lenguages/' + language + '.json');
+    const texts = await requestJson.json();
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        if(typeof texts[section][value] !== "object"){
+            textToChange.innerHTML = texts[section][value];
+        }else{
+            const ul = document.querySelector(".project2list");
+            ul.innerHTML ="";
+            texts[section][value].forEach(element => {
+                const li = document.createElement('li');
+                li.textContent = element["description"];
+                ul.appendChild(li);
+            });
+        }
+        
     }
-});
-
+    refreshCarousel();
+    refreshRol(texts["home"]["rolname"]);   
+}
+//REALIZA EL PARTIAL RENDER HTMLNAME: NOMBRE HTML, DOMCONTENTNAME: NOMBRE DE LA CLASE DONDE DEBE INSERTARSE EL HTML
 function partialRender(htmlName, domContentName) {
     return fetch("assets/html/" + htmlName)
         .then(response => response.text())
@@ -25,6 +46,7 @@ function partialRender(htmlName, domContentName) {
         .catch(error => console.error('Error al cargar el contenido:', error));
 }
 
+//INICIA CUANDO CARGA EL DOM
 document.addEventListener('DOMContentLoaded', function () {
     // Todo el DOM ha sido cargado y es seguro manipular los elementos del DOM aquí.
     runApp();
@@ -33,13 +55,11 @@ document.addEventListener('DOMContentLoaded', function () {
         topOffset: -70
     });
 });
-
 // Esta función inicializa la aplicación
 async function runApp() {
     // DETECTA EL LENGUAJE QUE ESTA UTILIZANDO EL USUARIO
-    const userLanguage = navigator.language || navigator.userLanguage;
-    const languageCode = userLanguage.split('-')[0];
-    // Realiza el partial render de la NavBar
+    const userLanguage = (navigator.language || navigator.userLanguage).split('-')[0];
+    // Realiza el partial render
     const renderPromises = [
         partialRender("navbar.html", "main-header"),
         partialRender("home.html", "home-section"),
@@ -47,62 +67,41 @@ async function runApp() {
         partialRender("tools.html", "tools-section"),
         partialRender("skills.html", "skill-section"),
         partialRender("portfolio.html", "portfolio-section"),
-        partialRender("portfolio.html", "portfolio-section"),
         partialRender("social.html", "social-section"),
         partialRender("contact.html", "contact-section"),
     ];
     await Promise.all(renderPromises);
-    // set del lenguaje correspondiente
-
     // Set del lenguaje correspondiente
-    if (languageCode.toLowerCase() == "es") {
-        readLanguage(false);
-    } else {
-        document.querySelector("#language-toggle").checked = true;
-        readLanguage(true);
-    }
-    
-    //Cambio el modo
+    const isSpanish = userLanguage.toLowerCase() === "es";
+    readLanguage(isSpanish);    
+    //Cambio los colores
     detectColorScheme();
-
     //agrego los addeventlistener
-    document.getElementById("language-toggle").addEventListener('change', (e) => readLanguage(e));
-    document.getElementById("mode-toggle").addEventListener('change', (e) => {
-        changeMode(e.target.checked);
-    });
-
+    document.getElementById("language-toggle").addEventListener('change', (e) => readLanguage(e.target.checked));
+    document.getElementById("mode-toggle").addEventListener('change', (e) => {changeMode(e.target.checked);});
 }
 
-
-
-//TEMAS
-
-function detectColorScheme() {
-    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (darkMode) {
-        changeMode(darkMode);
-    }
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-    changeMode(e.matches);
+//NAVBAR
+window.addEventListener("scroll", function () {
+    document.body.classList.toggle('fixed-header', window.scrollY >= 80);
 });
 
-function changeMode(checked) {
-    let label_toggle = document.getElementById('label-mode-toggle');
-    if (checked) {
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', 'dark');
-        label_toggle.innerHTML = "<i class='bx bx-sun' ></i>";
-        document.getElementById('mode-toggle').checked = true;
-    } else {
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', 'light');
-        label_toggle.innerHTML = "<i class='bx bx-moon' ></i>";
-        document.getElementById('mode-toggle').checked = false;
-    }
+//TEMAS
+//detecta que tema esta seleccionado
+function detectColorScheme() {
+    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    changeMode(darkMode);
 }
-
+//detecta si cambia el tema de la plataforma
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {changeMode(e.matches);});
+//CAMBIA EL COLOR SELECCIONADO
+function changeMode(mode) {
+    const theme = mode ? "dark" : "light";
+    document.documentElement.setAttribute('data-theme', theme);
+    document.getElementById('label-mode-toggle').innerHTML = mode ? "<i class='bx bx-sun'></i>" : "<i class='bx bx-moon'></i>";
+    document.getElementById('mode-toggle').checked = mode;
+}
+//SOCIAL
 function owlCarouselMain() {
     $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
@@ -131,18 +130,11 @@ function refreshCarousel() {
     owlCarouselMain();
 }
 
-
+//COPIA EL EMAIL AL PORTAPAPELES
 function copyEmail() {
-    // Obtener el valor del campo de texto
-    console.log("holaaa");
-    let text = "ivansamudio07@gmail.com";
     let notification = document.getElementById("notification");
-
-    navigator.clipboard.writeText(text).then(function () {
-        // Mostrar la notificación
+    navigator.clipboard.writeText("ivansamudio07@gmail.com").then(function () {
         notification.classList.add("show");
-
-        // Ocultar la notificación después de 2 segundos
         setTimeout(function () {
             notification.classList.remove("show");
         }, 2000);
@@ -151,41 +143,9 @@ function copyEmail() {
     });
 }
 
-
-function readLanguage(e) {
-    let checked = e;
-    if (typeof e != "boolean") {
-        checked = e.target.checked;
-    }
-    let label_toggle = document.getElementById('label-language-toggle');
-    if (checked) {
-        changeLenguage("en");
-        label_toggle.innerHTML = '<i>ES</i>';
-    } else {
-        changeLenguage("es");
-        label_toggle.innerHTML = '<i>EN</i>';
-    }
-}
-
-
-
-async function changeLenguage(language) {
-    const textsToChange = document.querySelectorAll("[data-section]");
-    const requestJson = await fetch('assets/lenguages/' + language + '.json');
-    const texts = await requestJson.json();
-    for (const textToChange of textsToChange) {
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
-        textToChange.innerHTML = texts[section][value];
-    }
-    refreshCarousel();
-    refreshRol(texts["home"]["rolname"]);
-    
-}
-
 function refreshRol(rolTexts){
     var typed = new Typed('#rol-changer', {
-        strings: [rolTexts[0],rolTexts[1]],
+        strings: rolTexts,
         typeSpeed: 100,
         loop: true,
     });
